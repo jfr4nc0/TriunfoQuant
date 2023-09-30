fe57ca3f58f9f7aa16af1d0d7fc37a74b473fddf
Joan Canossa <jcanossa@fusap.com.ar>
2023-09-30 13:36:27 -0300

    commit inicial

---
+++ src/main/java/com/jcanossa/triunfo/TriunfoApplication.java
@@ -0,0 +1,13 @@
+package com.jcanossa.triunfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriunfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriunfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jcanossa/triunfo/TriunfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcanossa.triunfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriunfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
